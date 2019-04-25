fc0fd18caa05170ee5f52323a906b13736f0222e
yolanda <yolan2olan@gmail.com>
2019-04-25 10:59:27 +0700

    update routes

--- routes/api.php
+++ routes/api.php
@@ -19,16 +19,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 //Route::apiResource('bugs','Bug\BugController');
 
-Route::post('ydh','Bug\BugController@store');
-Route::get('ydh','Bug\BugController@index');
-Route::post('ydh/{id}','Bug\BugController@update');
-Route::get('ydh/{id}','Bug\BugController@show');
-Route::delete('ydh/{id}', 'Bug\BugController@destroy');
-Route::post('ydh/search/result', 'Bug\BugController@search');
-
-
-Route::get('/news','NewsController@index');
-Route::post('/news','NewsController@store');
-Route::get('news/{id}','NewsController@show');
-Route::post('news/search/result', 'NewsController@search');
-Route::delete('news/{id}', 'NewsController@destroy');
+Route::post('bugs','Bug\BugController@store');
+Route::get('bugs','Bug\BugController@index');
+Route::post('bugs/{id}','Bug\BugController@update');
+Route::get('bugs/{id}','Bug\BugController@show');
+Route::delete('bugs/{id}', 'Bug\BugController@destroy');
+Route::post('bugs/search/result', 'Bug\BugController@search');
